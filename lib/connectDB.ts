8e54bb8f095876c3abdc9e99064bb164d84adb9d
comnk <vhsiao36@gmail.com>
2025-04-23 04:33:07 -0700

    local set up for Google OAuth connected with MongoDB working as intended

---
+++ lib/connectDB.ts
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+//const mongoPass = process.env.MONGO_PASS
+//const mongoUser = process.env. MONGO_USER
+//const DB_url = `mongodb+srv://${mongoUser}:${mongoPass}@dco-internal-tool.zyu6n.mongodb.net/DCo-Frontiers-2025?retryWrites=true&w=majority&appName=DCo-Internal-Tool`
+const DB_url = process.env.DATABASE_URL
+/*if(!mongoPass){
+    throw new Error(
+        'mongoose password is undefined'
+    );
+} else if (!mongoUser){
+    throw new Error(
+        'mongoose username is undefined'
+    );
+}*/
+
+type Cached = {
+    conn : mongoose.Connection | null; 
+    promise: Promise<mongoose.Connection> | null;
+} 
+
+declare global {
+    var mongoose: Cached;
+}
+
+let cached = global.mongoose;
+
+if(!cached){
+    cached = global.mongoose = {conn : null, promise : null}
+}
+
+async function connectDB() : Promise<mongoose.Connection> {
+    console.log("Connecting to MongoDB...");
+    if(cached.conn){
+        return cached.conn;
+    }
+
+    if(!cached.promise) {
+        const opts = {bufferCommands: false};
+
+        cached.promise = mongoose.connect(DB_url!, opts)
+        .then((mongoose) => {
+            return mongoose.connection;
+        });
+    }
+    
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
+
+export default connectDB;
