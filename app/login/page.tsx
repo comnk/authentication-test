725bb3cc797a7625e75cb877820da26c6cf2fb09
comnk <vhsiao36@gmail.com>
2025-04-23 07:42:33 -0700

    removed unnecessary useEffect

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { authClient } from "@/lib/auth-client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
